8dc92b2b098fdd911ce9b2cc3bc29a1ef95e1e53
Nathan Hall <nathan.mh@gmail.com>
2017-02-07 00:50:30 -0500

    load markers on map

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::PlacesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    place_ids = Favorite.where(user_id: User.all).map(&:place_id)
+    @places = Place.where(id: place_ids)
+    render json: @places, status: 200
   end
 
   def create
