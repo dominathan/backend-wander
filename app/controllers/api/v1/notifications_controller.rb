38b4f4909762b6a5bd4d71c39120f84a7cc0614b
Nathan Hall <nathan.mh@gmail.com>
2017-04-09 12:20:37 -0400

    private group handlign on notifications

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user_requests = User.joins(:groups).where(groups: {private: true, owner_id: @current_user.id}).where(group_users: { pending: true })
+    @user_requests = User.find_by_sql("SELECT users.*, group_users.group_id FROM users INNER JOIN group_users ON group_users.user_id = users.id INNER JOIN groups ON groups.id = group_users.group_id  WHERE users.id = #{@current_user.id} AND groups.private = #{true} AND group_users.pending = #{true}")
     render json: { status: 200, users: @user_requests }
   end
 end
