6393cf57b3d69cffc5afb8ebe2241fe247040c67
Nathan Hall <nathan.mh@gmail.com>
2017-04-23 09:51:15 -0400

    fix profile feed issue

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -26,13 +26,12 @@ class Api::V1::FeedsController < ApplicationController
 
   def feed_by_user
     if !params[:user_id]
-      @comments = Comment.where(user_id: @current_user.id)
+      comments = Comment.where(user_id: @current_user.id)
     else
-      @comments = Comment.where(user_id: User.find(params[:user_id]))
+      comments = Comment.where(user_id: User.find(params[:user_id]))
     end
-
-    @comments.order('created_at DESC').limit(100)
-             .map { |comment| {comment: comment.text, created_at: comment.created_at, user: comment.user, place: comment.place } }
+    @comments = comments.order('created_at DESC').limit(100)
+                        .map { |comment| {comment: comment.text, created_at: comment.created_at, user: comment.user, place: comment.place } }
     render json: @comments, status: 200
   end
 
