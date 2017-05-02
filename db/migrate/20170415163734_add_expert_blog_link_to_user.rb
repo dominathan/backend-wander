1a8b99d6a53a8e4527467b7afc065c1a54fffc64
Nathan Hall <nathan.mh@gmail.com>
2017-05-02 17:11:01 -0400

    fix migration

--- db/migrate/20170415163734_add_expert_blog_link_to_user.rb
+++ db/migrate/20170415163734_add_expert_blog_link_to_user.rb
@@ -1,5 +1,5 @@
 class AddExpertBlogLinkToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :expert_blog_log, :stringse
+    add_column :users, :expert_blog_log, :string
   end
 end
